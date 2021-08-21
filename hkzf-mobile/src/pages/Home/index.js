07bf9f3bdd2dd2eb9b45cd6de6bf889c552efa55
beitaandbaozi <2280496040@qq.com>
2021-08-21 16:25:58 +0800

    嵌套路由配置

--- hkzf-mobile/src/pages/Home/index.js
+++ hkzf-mobile/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import New from '../New'
+import { Route } from 'react-router'
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div style={{backgroundColor:'red'}}>
                 首页
+                {/* 渲染子路由 */}
+                <Route path="/home/new" component={New}></Route>
             </div>
         )
     }
---
+++ hkzf-mobile/src/pages/New/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class New extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor:'green'}}>
+                这是New组件的内容，子组件
+            </div>
+        )
+    }
+}
