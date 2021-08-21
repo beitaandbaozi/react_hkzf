8b6ecafab29a4fedaa89a8d31cd3e8516e237bf6
beitaandbaozi <2280496040@qq.com>
2021-08-21 15:10:23 +0800

    路由基础配置

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -1,7 +1,24 @@
-import {Button} from 'antd-mobile'
+import { Button } from 'antd-mobile'
+// 导入路由配置
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+// 导入首页和城市选择组件(2021-8-21)
+import Home from './pages/Home';
+import CityList from './pages/CityList';
+
 function App() {
   return (
-   <h1>App <Button>登录</Button></h1>
+    <BrowserRouter>
+
+      {/* 路由导航 */}
+      <ul>
+        <li><Link to='/home'>首页</Link></li>
+        <li><Link to='citylist'>城市选择</Link></li>
+      </ul>
+
+      {/* 路由注册 */}
+      <Route path='/home' component={Home}></Route>
+      <Route path='/citylist' component={CityList}></Route>
+    </BrowserRouter>
   );
 }
 
--- hkzf-mobile/src/index.js
+++ hkzf-mobile/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// 引入antd-mobile样式
+// 引入antd-mobile样式 （2021-8-21）
 import 'antd-mobile/dist/antd-mobile.css'
 
 
