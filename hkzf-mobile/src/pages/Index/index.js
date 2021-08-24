e27661ab3d90146061ad104a824057d93f64bb94
beitaandbaozi <2280496040@qq.com>
2021-08-24 20:40:44 +0800

    node-saa处理

--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -7,7 +7,7 @@ import Nav2 from '../../assets/images/nav-2.png'
 import Nav3 from '../../assets/images/nav-3.png'
 import Nav4 from '../../assets/images/nav-4.png'
 // 导入样式
-import './index.css'
+import './index.scss'
 
 export default class index extends Component {
     /**
