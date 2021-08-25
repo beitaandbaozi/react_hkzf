360499c632b275a0ed2d5c988028324a4b1d556c
beitaandbaozi <2280496040@qq.com>
2021-08-25 20:21:40 +0800

    城市选择页面顶部导航渲染

--- hkzf-mobile/src/pages/CityList/index.js
+++ hkzf-mobile/src/pages/CityList/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { NavBar,  } from 'antd-mobile';
+import './index.scss'
 
 export default class CityList extends Component {
     render() {
         return (
-            <div>
-                城市选择页面
+            // 顶部导航
+            <div className='cityselect'>
+                <NavBar
+                    className='nav'
+                    mode="light"
+                    icon={<i className='iconfont icon-back' />}
+                    onLeftClick={() => console.log(this.props.history.go(-1))}
+
+                >城市选择</NavBar>
             </div>
         )
     }
