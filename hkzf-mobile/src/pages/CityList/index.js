169ff8ff47879b5fcacc4d5871cd1efa07eb12c0
beitaandbaozi <2280496040@qq.com>
2021-08-25 21:01:58 +0800

    获取城市数据并转化

--- hkzf-mobile/src/pages/CityList/index.js
+++ hkzf-mobile/src/pages/CityList/index.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react'
-import { NavBar,  } from 'antd-mobile';
+import { NavBar, } from 'antd-mobile';
 import './index.scss'
+import axios from 'axios'
 
+// 处理获取的城市数据
+function formatCityData(list) {
+    // 键是首字母，值是一个数组：对应首字母的城市信息
+    let cityList = {}
+    list.forEach(item => {
+        // 通过简写获取到第一个首字母
+        let first = item.short.substr(0, 1)
+        // 判断对象中是否有这个key 我们可以利用对象取值的第二种方式 中括号的方式
+        if (cityList[first]) {
+            // 如果进入if 代表有这个值，我们只需要直接push进去
+            cityList[first].push(item)
+        } else {
+            // 如果进入else 代表没有这个值，我们初始化这个属性，并且把当前数据设置进去
+            cityList[first] = [item]
+        }
+    })
+    // 接下来我们需要把 cityList里面所有的key取出来，放在数组中，充当城市列表右侧的首字母导航条
+    let cityIndex = Object.keys(cityList).sort()
+    return {
+        cityList,
+        cityIndex
+    }
+}
 export default class CityList extends Component {
+
+    // 获取城市列表
+    async getCityList() {
+        const { data: { body } } = await axios.get('http://localhost:8080/area/city?level=1')
+        let { cityList, cityIndex } = formatCityData(body)
+        console.log(cityList, cityIndex);
+    }
+    componentDidMount() {
+        this.getCityList()
+    }
     render() {
         return (
             // 顶部导航
