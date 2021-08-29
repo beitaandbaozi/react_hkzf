7b766ae587d2b55a1a8dc6d69a6b2a86b4dfdafa
beitaandbaozi <2280496040@qq.com>
2021-08-29 16:19:14 +0800

    实现滚动对应索引高亮

--- hkzf-mobile/src/pages/CityList/index.js
+++ hkzf-mobile/src/pages/CityList/index.js
@@ -13,31 +13,31 @@ import { getCurrentCity } from '../../utils'
 // 数据格式化的方法
 // list: [{}, {}]
 const formatCityData = list => {
-  const cityList = {}
-  // const cityIndex = []
+    const cityList = {}
+    // const cityIndex = []
 
-  // 1 遍历list数组
-  list.forEach(item => {
-    // 2 获取每一个城市的首字母
-    const first = item.short.substr(0, 1)
-    // 3 判断 cityList 中是否有该分类
-    if (cityList[first]) {
-      // 4 如果有，直接往该分类中push数据
-      // cityList[first] => [{}, {}]
-      cityList[first].push(item)
-    } else {
-      // 5 如果没有，就先创建一个数组，然后，把当前城市信息添加到数组中
-      cityList[first] = [item]
+    // 1 遍历list数组
+    list.forEach(item => {
+        // 2 获取每一个城市的首字母
+        const first = item.short.substr(0, 1)
+        // 3 判断 cityList 中是否有该分类
+        if (cityList[first]) {
+            // 4 如果有，直接往该分类中push数据
+            // cityList[first] => [{}, {}]
+            cityList[first].push(item)
+        } else {
+            // 5 如果没有，就先创建一个数组，然后，把当前城市信息添加到数组中
+            cityList[first] = [item]
+        }
+    })
+
+    // 获取索引数据
+    const cityIndex = Object.keys(cityList).sort()
+
+    return {
+        cityList,
+        cityIndex
     }
-  })
-
-  // 获取索引数据
-  const cityIndex = Object.keys(cityList).sort()
-
-  return {
-    cityList,
-    cityIndex
-  }
 }
 
 /* 
@@ -80,134 +80,153 @@ const NAME_HEIGHT = 50
 
 // 封装处理字母索引的方法
 const formatCityIndex = letter => {
-  switch (letter) {
-    case '#':
-      return '当前定位'
-    case 'hot':
-      return '热门城市'
-    default:
-      return letter.toUpperCase()
-  }
+    switch (letter) {
+        case '#':
+            return '当前定位'
+        case 'hot':
+            return '热门城市'
+        default:
+            return letter.toUpperCase()
+    }
 }
 
 export default class CityList extends React.Component {
-  state = {
-    cityList: {},
-    cityIndex: [],
-    // 指定右侧字母索引列表高亮的索引号
-    activeIndex: 0
-  }
+    state = {
+        cityList: {},
+        cityIndex: [],
+        // 指定右侧字母索引列表高亮的索引号
+        activeIndex: 0
+    }
 
-  componentDidMount() {
-    this.getCityList()
-  }
+    componentDidMount() {
+        this.getCityList()
+    }
 
-  // 获取城市列表数据的方法
-  async getCityList() {
-    const res = await axios.get('http://localhost:8080/area/city?level=1')
-    const { cityList, cityIndex } = formatCityData(res.data.body)
+    // 获取城市列表数据的方法
+    async getCityList() {
+        const res = await axios.get('http://localhost:8080/area/city?level=1')
+        const { cityList, cityIndex } = formatCityData(res.data.body)
 
-    // 获取热门城市数据
-    const hotRes = await axios.get('http://localhost:8080/area/hot')
-    cityList['hot'] = hotRes.data.body
-    cityIndex.unshift('hot')
+        // 获取热门城市数据
+        const hotRes = await axios.get('http://localhost:8080/area/hot')
+        cityList['hot'] = hotRes.data.body
+        cityIndex.unshift('hot')
 
-    // 获取当前定位城市
-    const curCity = await getCurrentCity()
-    cityList['#'] = [curCity]
-    cityIndex.unshift('#')
+        // 获取当前定位城市
+        const curCity = await getCurrentCity()
+        cityList['#'] = [curCity]
+        cityIndex.unshift('#')
 
-    // console.log(cityList, cityIndex, curCity)
-    this.setState({
-      cityList,
-      cityIndex
-    })
-  }
+        // console.log(cityList, cityIndex, curCity)
+        this.setState({
+            cityList,
+            cityIndex
+        })
+    }
 
-  // List组件渲染每一行的方法：
-  rowRenderer = ({
-    key, // Unique key within array of rows
-    index, // 索引号
-    isScrolling, // 当前项是否正在滚动中
-    isVisible, // 当前项在 List 中是可见的
-    style // 注意：重点属性，一定要给每一个行数据添加该样式！作用：指定每一行的位置
-  }) => {
-    // 获取每一行的字母索引
-    const { cityIndex, cityList } = this.state
-    const letter = cityIndex[index]
+    // List组件渲染每一行的方法：
+    rowRenderer = ({
+        key, // Unique key within array of rows
+        index, // 索引号
+        isScrolling, // 当前项是否正在滚动中
+        isVisible, // 当前项在 List 中是可见的
+        style // 注意：重点属性，一定要给每一个行数据添加该样式！作用：指定每一行的位置
+    }) => {
+        // 获取每一行的字母索引
+        const { cityIndex, cityList } = this.state
+        const letter = cityIndex[index]
 
-    // 获取指定字母索引下的城市列表数据
-    // console.log(cityList[letter])
+        // 获取指定字母索引下的城市列表数据
+        // console.log(cityList[letter])
 
-    return (
-      <div key={key} style={style} className="city">
-        <div className="title">{formatCityIndex(letter)}</div>
-        {cityList[letter].map(item => (
-          <div className="name" key={item.value}>
-            {item.label}
-          </div>
-        ))}
-      </div>
-    )
-  }
+        return (
+            <div key={key} style={style} className="city">
+                <div className="title">{formatCityIndex(letter)}</div>
+                {cityList[letter].map(item => (
+                    <div className="name" key={item.value}>
+                        {item.label}
+                    </div>
+                ))}
+            </div>
+        )
+    }
 
-  // 创建动态计算每一行高度的方法
-  getRowHeight = ({ index }) => {
-    // 索引标题高度 + 城市数量 * 城市名称的高度
-    // TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
-    const { cityList, cityIndex } = this.state
-    return TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
-  }
+    // 创建动态计算每一行高度的方法
+    getRowHeight = ({ index }) => {
+        // 索引标题高度 + 城市数量 * 城市名称的高度
+        // TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
+        const { cityList, cityIndex } = this.state
+        return TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
+    }
 
-  // 封装渲染右侧索引列表的方法
-  renderCityIndex() {
-    // 获取到 cityIndex，并遍历其，实现渲染
-    const { cityIndex, activeIndex } = this.state
-    return cityIndex.map((item, index) => (
-      <li className="city-index-item" key={item}>
-        <span className={activeIndex === index ? 'index-active' : ''}>
-          {item === 'hot' ? '热' : item.toUpperCase()}
-        </span>
-      </li>
-    ))
-  }
+    // 封装渲染右侧索引列表的方法
+    renderCityIndex() {
+        // 获取到 cityIndex，并遍历其，实现渲染
+        const { cityIndex, activeIndex } = this.state
+        return cityIndex.map((item, index) => (
+            <li className="city-index-item" key={item}>
+                <span className={activeIndex === index ? 'index-active' : ''}>
+                    {item === 'hot' ? '热' : item.toUpperCase()}
+                </span>
+            </li>
+        ))
+    }
 
-  render() {
-    return (
-      <div className="citylist">
-        {/* 顶部导航栏 */}
-        <NavBar
-          className="navbar"
-          mode="light"
-          icon={<i className="iconfont icon-back" />}
-          onLeftClick={() => this.props.history.go(-1)}
-        >
-          城市选择
-        </NavBar>
+    /**
+     * 给 List 组件添加 onRowsRendered 配置项, 用于获取当前列表渲染的行信息
+     * 通过参数 startIndex 获取到, 起始行索引(也就是城市列表可视区最顶部一行的索引号)
+     * 判断 startIndex 和 activeIndex 是否相同(判断的目的是为了提升性能, 避免不必要的state更新)
+     * 当 startIndex 和 activeIndex 不同时, 更新状态 activeIndex 为 startIndx 的值 
+     */
 
-        {/* 城市列表 */}
-        <AutoSizer>
-          {({ width, height }) => (
-            <List
-              width={width}
-              height={height}
-              rowCount={this.state.cityIndex.length}
-              rowHeight={this.getRowHeight}
-              rowRenderer={this.rowRenderer}
-            />
-          )}
-        </AutoSizer>
 
-        {/* 右侧索引列表 */}
-        {/* 
+    //   用于获取List组件渲染行的信息
+    onRowsRendered = ({ startIndex }) => {
+        if (this.state.activeIndex !== startIndex) {
+            this.setState({
+                activeIndex: startIndex
+            })
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="citylist">
+                {/* 顶部导航栏 */}
+                <NavBar
+                    className="navbar"
+                    mode="light"
+                    icon={<i className="iconfont icon-back" />}
+                    onLeftClick={() => this.props.history.go(-1)}
+                >
+                    城市选择
+                </NavBar>
+
+                {/* 城市列表 */}
+                <AutoSizer>
+                    {({ width, height }) => (
+                        <List
+                            width={width}
+                            height={height}
+                            rowCount={this.state.cityIndex.length}
+                            rowHeight={this.getRowHeight}
+                            rowRenderer={this.rowRenderer}
+                            onRowsRendered={this.onRowsRendered}
+                        />
+                    )}
+                </AutoSizer>
+
+                {/* 右侧索引列表 */}
+                {/* 
           1 封装 renderCityIndex 方法，用来渲染城市索引列表。
           2 在方法中，获取到索引数组 cityIndex ，遍历 cityIndex ，渲染索引列表。
           3 将索引 hot 替换为 热。
           4 在 state 中添加状态 activeIndex ，指定当前高亮的索引。
           5 在遍历 cityIndex 时，添加当前字母索引是否高亮的判断条件。
         */}
-        <ul className="city-index">{this.renderCityIndex()}</ul>
-      </div>
-    )
-  }
+                <ul className="city-index">{this.renderCityIndex()}</ul>
+            </div>
+        )
+    }
 }
