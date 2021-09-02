3b5b4e23cf7733a115248aa168c1ae6b45d9c794
beitaandbaozi <2280496040@qq.com>
2021-09-02 13:44:54 +0800

    房源信息文本绘制

--- hkzf-mobile/src/pages/Map/index.js
+++ hkzf-mobile/src/pages/Map/index.js
@@ -7,19 +7,28 @@ import styles from './index.module.css'
 // 引入组件
 import NavHead from '../../components/NavHead'
 //  解决脚手架全局变量访问的问题
-const BMapGL  = window.BMapGL
-
+const BMapGL = window.BMapGL
+// 覆盖物样式
+const labelStyle = {
+    cursor: 'pointer',
+    border: '0px solid rgb(255, 0, 0)',
+    padding: '0px',
+    whiteSpace: 'nowrap',
+    fontSize: '12px',
+    color: 'rgb(255, 255, 255)',
+    textAlign: 'center'
+  }
 
 export default class index extends Component {
     // 初始化地图
-    initMap(){
-            /**
-         * 根据定位展示当前城市
-         * 1.获取当前定位城市
-         * 2.使用地址解析器解析当前城市坐标
-         * 3.调用 centerAndZoom() 方法在地图中展示当前城市，并设置缩放级别为11
-         * 4.在地图中展示该城市，并添加比例尺和平移缩放控件
-         */
+    initMap() {
+        /**
+     * 根据定位展示当前城市
+     * 1.获取当前定位城市
+     * 2.使用地址解析器解析当前城市坐标
+     * 3.调用 centerAndZoom() 方法在地图中展示当前城市，并设置缩放级别为11
+     * 4.在地图中展示该城市，并添加比例尺和平移缩放控件
+     */
 
         // 获取当前定位的城市
         // localStorage里面是字符串 记得要转化
@@ -52,14 +61,31 @@ export default class index extends Component {
                  * 2.调用 setStyle() 方法设置样式
                  * 3.在 map 对象上调用 addOverlay() 方法，将文本覆盖物添加到地图中
                  */
+
+                /**
+                 * 绘制房源覆盖物
+                 * 1.调用 Label 的 setContent() 方法，传入HTML结构，修改 HTML内容的样式
+                 * 2.调用 setStyle() 修改覆盖物样式
+                 * 3.为文本覆盖物添加单击事件
+                 */
+
+
                 const opts = {
-                    position:point
+                    position: point,
+                    offset: new BMapGL.Size(-35, -35)
                 }
-                const label = new BMapGL.Label('beita',opts)
+                const label = new BMapGL.Label('', opts)
+
+                //绘制
+                label.setContent(`
+                <div class="${styles.bubble}">
+                    <p class="${styles.name}">广州</p>
+                    <p>7套</p>
+                </div>
+                `)
+
                 // 设置样式
-                label.setStyle({
-                    color:'lightblue'
-                })
+                label.setStyle(labelStyle)
                 // 添加覆盖物到地图中
                 map.addOverlay(label)
 
