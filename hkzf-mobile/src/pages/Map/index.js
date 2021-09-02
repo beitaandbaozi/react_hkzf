e9b74003dcc0a7eb1990fd3adc07292560d9a9ec
beitaandbaozi <2280496040@qq.com>
2021-09-02 11:20:55 +0800

    根据定位（之前存储到localStorage中）获取对应城市的地图

--- hkzf-mobile/src/pages/Map/index.js
+++ hkzf-mobile/src/pages/Map/index.js
@@ -6,15 +6,56 @@ import styles from './index.module.css'
 
 // 引入组件
 import NavHead from '../../components/NavHead'
+//  解决脚手架全局变量访问的问题
+const BMapGL  = window.BMapGL
+
 
 export default class index extends Component {
-    componentDidMount(){
+    // 初始化地图
+    initMap(){
+            /**
+         * 根据定位展示当前城市
+         * 1.获取当前定位城市
+         * 2.使用地址解析器解析当前城市坐标
+         * 3.调用 centerAndZoom() 方法在地图中展示当前城市，并设置缩放级别为11
+         * 4.在地图中展示该城市，并添加比例尺和平移缩放控件
+         */
+
+        // 获取当前定位的城市
+        // localStorage里面是字符串 记得要转化
+        const { label, value } = JSON.parse(localStorage.getItem('localCity'))
+        console.log(label, value);
+
+
         // 初始化地图实例
-        const map = new window.BMapGL.Map("container");
+        const map = new BMapGL.Map("container");
         // 设置中心点
-        const  point = new window.BMapGL.Point(116.404, 39.915);
+        // const point = new window.BMapGL.Point(116.404, 39.915);
+
+        //创建地址解析器实例
+        const myGeo = new BMapGL.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(label, (point) => {
+            if (point) {
+                // 初始化地图
+                map.centerAndZoom(point, 11);
+                // 标记物
+                // map.addOverlay(new BMapGL.Marker(point))
+
+                // 添加常用控件
+                map.addControl(new BMapGL.ScaleControl())
+                map.addControl(new BMapGL.ZoomControl())
+            } else {
+                alert('您选择的地址没有解析到结果！');
+            }
+        }, label)
+
+
         // 初始化地图
-        map.centerAndZoom(point, 15); 
+        // map.centerAndZoom(point, 15);
+    }
+    componentDidMount() {
+        this.initMap()
     }
     render() {
         return (
