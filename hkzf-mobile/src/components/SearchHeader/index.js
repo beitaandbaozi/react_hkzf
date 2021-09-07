cbd4993bb858ddb5396eb2e1c51db4adefab034e
beitaandbaozi <2280496040@qq.com>
2021-09-07 21:15:36 +0800

    封装首页搜索栏为组件

---
+++ hkzf-mobile/src/components/SearchHeader/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Flex } from 'antd-mobile'
+import {withRouter} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import './index.scss'
+
+
+function SearchHeader({history,cityName}) {
+    return (
+        <div>
+            {/* 搜索框 */}
+            <Flex className='search-box'>
+                {/* 左侧白色区域 */}
+                <Flex className="search">
+                    {/* 位置 */}
+                    <div className="location" onClick={() => history.push('/citylist')}>
+                        <span className="name">{cityName}</span>
+                        <i className="iconfont icon-arrow" />
+                    </div>
+
+                    {/* 搜索表单 */}
+                    <div className="form" onClick={() => history.push('/search')}>
+                        <i className="iconfont icon-seach" />
+                        <span className="text">请输入小区或地址</span>
+                    </div>
+                </Flex>
+                {/* 右侧地图图标 */}
+                <i className="iconfont icon-map" onClick={() => history.push('/map')} />
+            </Flex>
+        </div>
+    )
+}
+
+// 添加校验规则
+SearchHeader.propTypes = {
+    cityName: PropTypes.string.isRequired
+}
+
+export default withRouter(SearchHeader)
--- hkzf-mobile/src/pages/HouseList/index.js
+++ hkzf-mobile/src/pages/HouseList/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import SearchHeader from '../../components/SearchHeader'
+
+
+// 获取当前定位城市信息
+const { label } = JSON.parse(localStorage.getItem('localCity'))
 
 export default class HouseList extends Component {
     render() {
         return (
             <div>
-                HouseList
+                <SearchHeader cityName={label} />
             </div>
         )
     }
