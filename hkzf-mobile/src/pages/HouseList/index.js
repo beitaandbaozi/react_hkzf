8ee21de3d71b26e14fffec989d7f8207e8a5fdc9
beitaandbaozi <2280496040@qq.com>
2021-09-08 20:31:37 +0800

    建立条件筛选文件和部署静态布局和样式

--- hkzf-mobile/src/pages/HouseList/index.js
+++ hkzf-mobile/src/pages/HouseList/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import SearchHeader from '../../components/SearchHeader'
 import { Flex } from 'antd-mobile'
 import styles from './index.module.css'
+// 引入筛选组件
+import Filter from './components/Filter'
 
 // 获取当前定位城市信息
 const { label } = JSON.parse(localStorage.getItem('localCity'))
@@ -14,6 +16,8 @@ export default class HouseList extends Component {
                     <i className='iconfont icon-back' onClick={()=> this.props.history.go(-1)}></i>
                     <SearchHeader cityName={label} className={styles.searchHeader}/>
                 </Flex> 
+
+                <Filter />
             </div>
         )
     }
